ed7cac2f2ea50cbd7d1a2aadfbeac53aaca59c92
fire-h0und <55053506+fire-h0und@users.noreply.github.com>
2021-09-07 18:24:04 +0200

    Add json file and empty library (#1)
    * Defined the library with an empty class
    
    * Added the numpad json file
    
    It is posibly inconsistent and full of errors, but it's a start.
    
    * Fixed comments in Keyson.hx
    
    Co-authored-by: Stevan C <cest@block.eth.lan>
    Co-authored-by: logo4poop <logo4poop@protonmail.com>

---
+++ src/Keyson.hx
@@ -0,0 +1,15 @@
+package keyson;
+
+/*
+ * theKeyboardEditor file interfacing library
+ * https://github.com/theKeyboardEditor/keyson
+ */
+
+@:structInit
+typedef KeysonOptions = {
+	// TODO
+}
+
+class Keyson {
+	// TODO
+}
