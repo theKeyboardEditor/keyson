f08e5ec6c6d0368139df7aad4a2e89a07abfae8d
Stevan C <cest@block.eth.lan>
2021-09-07 18:15:58 +0200

    Defined the library with an empty class

---
+++ src/keyson.hx
@@ -0,0 +1,13 @@
+package keyson;
+
+// the Keyboar Editor file interfecing library
+// https://github.com/theKeyboardEditor/keyson
+
+@:structInit
+typedef KeysonOptions = {
+	// TODO
+}
+
+class Keyson {
+	// TODO
+}
