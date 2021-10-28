da9d56b2559480fb1c1c28d77a870240d32fd75c
Isaac Silverstein <logo4poop@protonmail.com>
2021-10-27 22:17:33 -0400

    Added an extra test, renamed tests folder

--- test/TestCase.hx
+++ test/TestCase.hx
@@ -24,9 +24,28 @@ class TestCase extends utest.Test {
 	function testKeyAdding() {
 		var keeb = Keyson.parse(keyboard);
 
-		keeb.board[0].addKey("1U",[12,12],"Test");
+		keeb.board[0].addKey("1U", [12,12], "Test");
 
 		// Check if there is eighteen keys after we added one
 		Assert.equals(18, keeb.board[0].keys.length);
+	}
+
+	function testKeyboardFromScratch() {
+		var keeb = new Keyson();
+
+		keeb.name = "TestKeyboard";
+		
+		keeb.board[0].addKey("1U", [0, 0], "Esc");
+		keeb.board[0].addKey("1U", [1, 0], "1");
+		keeb.board[0].addKey("1U", [2, 0], "2");
+		keeb.board[0].addKey("1U", [3, 0], "3");
+		keeb.board[0].addKey("1U", [4, 0], "4");
+		keeb.board[0].addKey("2U", [5, 0], "Backspace");
+
+		// Check if it contains all 6 keys
+		Assert.equals(6, keeb.board[0].keys.length-1);
+		Assert.equals("TestKeyboard", keeb.name);
+
+		trace(Keyson.encode(keeb));
 	}
 }
